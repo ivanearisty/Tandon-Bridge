f10cfc2c85d3d62954d9a207401f8986067b86e9
ivanearisty <ivanearisty@gmail.com>
2024-01-29 19:50:44 -0500

    Finished Homework

--- HomeWork4/iae225.hw4.q7.cpp
+++ HomeWork4/iae225.hw4.q7.cpp
@@ -2,9 +2,21 @@
 # include <vector>
 using namespace std;
 
+/*Write a program that reads a positive integer n from the user and prints out a nxn
+multiplication table. The columns should be spaced by a tab.*/
+
 int main (){
 
-        vector<int> msg;
+	int n = 0;
+	cout << "Please enter a positive integer: ";
+	cin >> n;
+
+	for(int i = 1 ; i <= n ; i++){
+		for(int j = 1 ; j <= n ; j++){
+			cout << i * j << "\t";
+		}
+		cout << endl;
+	}
 
     return 0;
 }
